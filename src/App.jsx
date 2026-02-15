c211ec2a69ccf8d34f923b575546f1e3718764a6
Suriya Pinitkan <116714861+suriyapi@users.noreply.github.com>
2026-02-15 10:58:27 +0700

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import { useState } from 'react'
 import './App.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight, faArrowsDownToLine } from '@fortawesome/free-solid-svg-icons'
-import { faSquareInstagram } from '@fortawesome/free-brands-svg-icons'
-import { faSquareYoutube } from '@fortawesome/free-brands-svg-icons'
-import { faGit } from '@fortawesome/free-brands-svg-icons'
 
-import fileResume from './assets/cv.pdf'
+import Header from './section/Header'
+import Navbar from './section/Navbar'
+import Contact from './section/Contact'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,35 +12,9 @@ function App() {
     <div className='mt-14 mx-auto max-w-6xl grid grid-cols-[40%_60%]'>
       <div>
         <div className='sticky top-14 grid grid-rows-[35%_40%_25%] h-[90vh]'>
-          <div className='flex flex-col gap-2'>
-            <div className='text-3xl text-primaryTitle'>Suriya Pinitkan</div>
-            <div className='text-primaryAccent font-semibold'>Software Developer · between jobs</div>
-            <div className='text-sm w-5/6'>From concept to reality, bridging the gap with code, transforming ideas into tangible digital solutions.</div>
-            <div className='mt-4'>
-              <a href={fileResume} target='_blank'>
-                <span className='rounded-md bg-primaryTitle text-white py-2 px-4'>
-                  View Resume
-                  <span className='rotate-90 inline-block ml-2 text-sm'>
-                    <FontAwesomeIcon className='animate-bounce' icon={faArrowsDownToLine} />
-                  </span>
-                </span>
-              </a>
-            </div>
-          </div>
-          <div className='flex flex-col gap-4 font-semibold'>
-            <div>
-              <FontAwesomeIcon className='mr-2 text-primaryTitle' icon={faArrowRight}/>
-              About
-              </div>
-            <div>Experience</div>
-            <div>Projects</div>
-            <div>Article</div>
-          </div>
-          <div className='flex items-end gap-2 text-2xl '>
-              <FontAwesomeIcon className='hover:scale-125 hover:text-primaryTitle transition-all' icon={faSquareInstagram} />
-              <FontAwesomeIcon className='hover:scale-125 hover:text-primaryTitle transition-all' icon={faSquareYoutube} />
-              <FontAwesomeIcon className='hover:scale-125 hover:text-primaryTitle transition-all' icon={faGit} />
-          </div>
+          <Header />
+          <Navbar />
+          <Contact />
         </div>
       </div>
       <div>
---
+++ src/section/Header/index.jsx
@@ -0,0 +1,25 @@
+import fileResume from '../../assets/cv.pdf'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight, faArrowsDownToLine } from '@fortawesome/free-solid-svg-icons'
+
+const Header = () => {
+    return (
+        <div className='flex flex-col gap-2'>
+            <div className='text-3xl text-primaryTitle'>Suriya Pinitkan</div>
+            <div className='text-primaryAccent font-semibold'>Software Developer · between jobs</div>
+            <div className='text-sm w-5/6'>From concept to reality, bridging the gap with code, transforming ideas into tangible digital solutions.</div>
+            <div className='mt-4'>
+                <a href={fileResume} target='_blank'>
+                    <span className='rounded-md bg-primaryTitle text-white py-2 px-4'>
+                        View Resume
+                        <span className='rotate-90 inline-block ml-2 text-sm'>
+                            <FontAwesomeIcon className='animate-bounce' icon={faArrowsDownToLine} />
+                        </span>
+                    </span>
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
